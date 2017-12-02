9c5e67e2711b73cea16d729ba5575e7569867c2d
Johannes <johannesbruehl@googlemail.com>
2017-12-02 11:48:30 +0100

    Added Basic HelloWorld Server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import Network.Socket
 
 main :: IO ()
-main = someFunc
+main  = do
+    putStr "In Main"
+    sock <- socket AF_INET Stream 0    -- create socket
+    setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
+    bind sock (SockAddrInet 8080 iNADDR_ANY)   -- listen on TCP port 4242.
+    listen sock 2                              -- set a max of 2 queued connections
+    mainLoop sock
+
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+    conn <- accept sock     -- accept a connection and handle it
+    runConn conn            -- run our server's logic
+    mainLoop sock           -- repeat
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+    send sock "Hello!\n"
+    close sock
