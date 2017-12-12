49d8ff3a3918bcacf905e32abf94f948bf4ff41f
Florian Pirchmoser <pirchmos@hm.edu>
2017-12-12 17:03:00 +0100

    Yeeehawwww

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,45 @@
 module Main where
 
 import Network.Socket
+data HttpStatusCode = Ok | NotFound
+  deriving(Show, Enum)
+
+code :: HttpStatusCode -> Int
+reasonPhrase :: HttpStatusCode -> [Char]
+
+code Ok = 200
+code NotFound = 404
+reasonPhrase Ok = "OK"
+reasonPhrase NotFound = "NOT FOUND"
+
+data ResponseLine = ResponseLine String HttpStatusCode
+
+instance Show ResponseLine where
+  show ( ResponseLine version status) = version ++ " " ++ show (code status) ++ " " ++ reasonPhrase status
+
+okLine = ResponseLine "HTTP/1.1" Ok
+
+port = 8080
 
 main :: IO ()
 main  = do
-    putStr "In Main"
-    sock <- socket AF_INET Stream 0    -- create socket
-    setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
-    bind sock (SockAddrInet 8080 iNADDR_ANY)   -- listen on TCP port 4242.
-    listen sock 2                              -- set a max of 2 queued connections
-    mainLoop sock
+      sock <- socket AF_INET Stream 0    -- create socket
+      setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
+      putStrLn $ "Binding to socket on port " ++ show port
+      bind sock (SockAddrInet port iNADDR_ANY)   -- listen on TCP port 4242.
+      listen sock 2                              -- set a max of 2 queued connections
+      mainLoop sock
 
-mainLoop :: Socket -> IO ()
+mainLoop :: Socket -> IO()
 mainLoop sock = do
-    conn <- accept sock     -- accept a connection and handle it
-    runConn conn            -- run our server's logic
-    mainLoop sock           -- repeat
+  conn <- accept sock
+  putStrLn "Accepting connection"
+  run conn
+  mainLoop sock
 
-runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, _) = do
-    send sock "Hello!\n"
-    close sock
+run :: (Socket, SockAddr) -> IO()
+run (sock, _) = do
+  putStr "\nSending message\n"
+  putStrLn $ show okLine
+  send sock $ show okLine ++ "\r\nContent-Length: 3\r\nContent-Type: text/plain\r\n\r\nHi!"
+  close sock
