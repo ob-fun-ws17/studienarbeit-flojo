31370866fa707f59bbc94f97d6aee90866337cce
Florian Pirchmoser <pirchmos@hm.edu>
2018-01-09 00:05:00 +0100

    Simple read implementation printing IO errors

--- src/Read.hs
+++ src/Read.hs
@@ -1,8 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Read (
   read
 )
-  where
-    import Data.ByteString
+where
+import Prelude hiding (read)
+import qualified Data.ByteString as BS
+import System.Directory
+import Control.Monad
+import Control.Exception (catch)
 
-    read :: FilePath -> IO ByteString
-    read path = readFile path
+read :: String -> IO (Either IOError BS.ByteString)
+read path = catch( do
+  result <- BS.readFile path
+  return $ Right result
+  ) handler
+  where
+    handler :: IOError -> IO (Either IOError BS.ByteString)
+    handler ex = do
+      putStrLn $ "Caught exception: " ++ show ex
+      return $ Left ex
